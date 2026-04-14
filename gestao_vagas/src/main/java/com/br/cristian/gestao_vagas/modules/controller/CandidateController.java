064d450c48e90581df2929e1e37171d4a50cd3a7
Cristian Meira <cristian.meira15@gmail.com>
2026-04-13 23:07:43 -0300

    added validation and updated the archive pom.xml

--- gestao_vagas/src/main/java/com/br/cristian/gestao_vagas/modules/controller/CandidateController.java
+++ gestao_vagas/src/main/java/com/br/cristian/gestao_vagas/modules/controller/CandidateController.java
@@ -1,16 +1,20 @@
 package com.br.cristian.gestao_vagas.modules.controller;
 
 
+import com.br.cristian.gestao_vagas.modules.models.Candidate;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/candidate")
-public class candidateController {
+public class CandidateController {
 
     @PostMapping("/")
-    public void create(){
-
+    public void create(@Valid @RequestBody Candidate candidate){
+        System.out.println("Candidate: ");
+        System.out.println(candidate.getEmail());
     }
 }
--- gestao_vagas/src/main/java/com/br/cristian/gestao_vagas/modules/models/Candidate.java
+++ gestao_vagas/src/main/java/com/br/cristian/gestao_vagas/modules/models/Candidate.java
@@ -1,12 +1,23 @@
 package com.br.cristian.gestao_vagas.modules.models;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 import java.util.UUID;
 
-public class candidate {
+@Data
+public class Candidate {
 
     private UUID id;
     private String name;
+
+    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaços")
     private String username;
+
+    @Email(message = "O campo [email] deve conter um e-mail válido.")
     private String email;
+
+    @Length(min = 6, max = 20)
     private String password;
     private String description;
     private String curriculum;
