6953e15f4fca17f8fac61634ac63936b0afb5c12
Cristian Meira <cristian.meira15@gmail.com>
2026-04-14 10:40:59 -0300

    adapted canditade controller to use service for validations

--- gestao_vagas/src/main/java/com/br/cristian/gestao_vagas/modules/controller/CandidateController.java
+++ gestao_vagas/src/main/java/com/br/cristian/gestao_vagas/modules/controller/CandidateController.java
@@ -1,8 +1,13 @@
 package com.br.cristian.gestao_vagas.modules.controller;
 
 
+import com.br.cristian.gestao_vagas.modules.exception.UserFoundException;
 import com.br.cristian.gestao_vagas.modules.models.Candidate;
+import com.br.cristian.gestao_vagas.modules.repository.CandidateRepository;
+import com.br.cristian.gestao_vagas.modules.service.CandidateService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/candidate")
 public class CandidateController {
 
+    @Autowired
+    private CandidateService candidateService;
+
     @PostMapping("/")
-    public void create(@Valid @RequestBody Candidate candidate){
-        System.out.println("Candidate: ");
-        System.out.println(candidate.getEmail());
+    public ResponseEntity<Object> create(@Valid @RequestBody Candidate candidateEntity){
+        try{
+            var result =  this.candidateService.createCandidate(candidateEntity);
+            return ResponseEntity.ok().body(result);
+        } catch(Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
+
+
 }
