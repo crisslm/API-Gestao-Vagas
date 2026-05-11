d88983ca2a5f8aa9b1f58ab9fd6dfd5ec5948f1b
Cristian Meira <cristian.meira15@gmail.com>
2026-05-11 14:09:53 -0300

    DTO to restrict what the user can input

---
+++ gestao_vagas/src/main/java/com/br/cristian/gestao_vagas/modules/dto/AuthCompanyDTO.java
@@ -0,0 +1,14 @@
+package com.br.cristian.gestao_vagas.modules.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthCompanyDTO {
+
+    private String username;
+    private String password;
+
+}
